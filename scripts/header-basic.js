5a52f3f4a8cf8b72b36e720735cf91d0e49d25de
Jhony <xxxpickxxx@gmail.com>
2024-05-12 08:24:40 +0300

    finish pinned header

--- scripts/header-basic.js
+++ scripts/header-basic.js
@@ -1,15 +1,43 @@
-const checkbox = document.querySelector(
-  '.language_switch input[type="checkbox"]'
-);
-const uaSpan = document.getElementById("language_switch-ua");
-const ruSpan = document.getElementById("language_switch-ru");
+const checkboxes = document.querySelectorAll(".language_switch-input");
 
-checkbox.addEventListener("change", function () {
-  if (checkbox.checked) {
-    ruSpan.classList.add("active");
-    uaSpan.classList.remove("active");
-  } else {
-    uaSpan.classList.add("active");
-    ruSpan.classList.remove("active");
-  }
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener("change", function () {
+    const isChecked = checkbox.checked;
+    checkboxes.forEach((otherCheckbox) => {
+      if (otherCheckbox !== checkbox) {
+        otherCheckbox.checked = isChecked;
+      }
+    });
+
+    const uaSpans = document.querySelectorAll(".language_switch-span-ua");
+    const ruSpans = document.querySelectorAll(".language_switch-span-ru");
+
+    if (isChecked) {
+      ruSpans.forEach((span) => span.classList.add("active"));
+      uaSpans.forEach((span) => span.classList.remove("active"));
+    } else {
+      uaSpans.forEach((span) => span.classList.add("active"));
+      ruSpans.forEach((span) => span.classList.remove("active"));
+    }
+  });
+});
+
+window.addEventListener("scroll", function () {
+  var headerBottoms = document.querySelectorAll(".header_bottom");
+  var headerTabs = document.querySelectorAll(".common-template__tabs");
+  var tabs = document.querySelectorAll(".tabs");
+  var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+  var isFixed = scrollPosition > 64;
+
+  headerBottoms.forEach(function (headerBottom) {
+    headerBottom.classList.toggle("fixed", isFixed);
+  });
+
+  headerTabs.forEach(function (headerTab) {
+    headerTab.classList.toggle("fixed", isFixed);
+  });
+
+  tabs.forEach(function (headerTab) {
+    headerTab.classList.toggle("tabs-fixed", isFixed);
+  });
 });
