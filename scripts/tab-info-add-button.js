6336caeb0e5dcb92c1cdf4927fd4e863b6ff0f4f
Jhony <xxxpickxxx@gmail.com>
2024-05-12 09:55:45 +0300

    add status product and button

--- scripts/tab-info-add-button.js
+++ scripts/tab-info-add-button.js
@@ -2,3 +2,18 @@ function toggleTabInfoBtn(event, button) {
   event.preventDefault();
   button.classList.toggle("active");
 }
+function toggleBayBtn(event, button) {
+  event.preventDefault();
+  button.classList.toggle("global-button-buy");
+  button.classList.toggle("active");
+
+  // Получаем текущий текст кнопки
+  var currentText = button.textContent.trim();
+
+  // Устанавливаем новый текст в зависимости от текущего
+  if (currentText === "Купити") {
+    button.textContent = "В кошику";
+  } else {
+    button.textContent = "Купити";
+  }
+}
