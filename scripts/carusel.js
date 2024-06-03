dd6639b69096fab232dc02efeab6d1a70893d4d9
Jhony <xxxpickxxx@gmail.com>
2024-06-04 02:30:52 +0300

    fix with

--- scripts/carusel.js
+++ scripts/carusel.js
@@ -1,4 +1,31 @@
 $(document).ready(function () {
+  function updateCarouselWrapperClass() {
+    var $carouselWrapper = $(".gen-tab__add-product-accessories-container");
+    if ($(window).width() <= 1279) {
+      $carouselWrapper.addClass("carousel-wrapper-min");
+      $carouselWrapper.removeClass("carousel-wrapper");
+
+      $(".carousel-wrapper-min").slick({
+        infinite: false,
+        slidesToShow: 4.5,
+        slidesToScroll: 1,
+        arrows: false,
+      });
+    } else {
+      $carouselWrapper.removeClass("carousel-wrapper-min");
+      $carouselWrapper.addClass("carousel-wrapper");
+
+      $(".carousel-wrapper").slick({
+        infinite: false,
+        slidesToShow: 3.2,
+        slidesToScroll: 1,
+        arrows: false,
+      });
+    }
+  }
+
+  $(window).on("resize", updateCarouselWrapperClass);
+  $(document).ready(updateCarouselWrapperClass);
   // Инициализация слайдера
   $(".gen-tab__carousel-wrapper").slick({
     infinite: false,
@@ -18,12 +45,19 @@ $(document).ready(function () {
 
   //Аксы
   // Инициализация слайдера
-  $(".carousel-wrapper").slick({
-    infinite: false,
-    slidesToShow: 3.2,
-    slidesToScroll: 1,
-    arrows: false,
-  });
+  // $(".carousel-wrapper").slick({
+  //   infinite: false,
+  //   slidesToShow: 3.2,
+  //   slidesToScroll: 1,
+  //   arrows: false,
+  // });
+
+  // $(".carousel-wrapper-min").slick({
+  //   infinite: false,
+  //   slidesToShow: 4,
+  //   slidesToScroll: 1,
+  //   arrows: false,
+  // });
 
   // Привязка кнопок к слайдеру
   $(".gen-tab__prev").click(function () {
