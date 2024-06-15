b9529fffe01a9558eedeea72a0e18ddf30af9c6a
Jhony <xxxpickxxx@gmail.com>
2024-06-15 11:17:06 +0300

    end adaptive

--- scripts/button-add.js
+++ scripts/button-add.js
@@ -3,3 +3,12 @@ function toggleAddAcessBtn(event, button) {
   button.classList.toggle("add-product");
   button.classList.toggle("success");
 }
+
+function toggleShowMoreBtn(event) {
+  event.preventDefault();
+  const button = event.target;
+  const showMoreContainer = button.closest(".show-more");
+  if (showMoreContainer) {
+    showMoreContainer.classList.toggle("show");
+  }
+}
