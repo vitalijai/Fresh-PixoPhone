ede962ee4fd86b070b7634b900492196160edbf6
ZigfeR <xxxpickxxx@gmail.com>
2024-07-30 08:38:56 +0300

    finished exclusive-recently

--- scripts/button-add.js
+++ scripts/button-add.js
@@ -16,19 +16,19 @@ function toggleShowMoreBtn(event) {
 var containers = document.querySelectorAll(".global-block-content-container");
 
 // Перебираем каждый контейнер
-containers.forEach(function (container) {
-  // Получаем все кнопки в текущем контейнере
-  var buttons = container.querySelectorAll("button");
+// containers.forEach(function (container) {
+//   // Получаем все кнопки в текущем контейнере
+//   var buttons = container.querySelectorAll("button");
 
-  // Перебираем каждую кнопку
-  buttons.forEach(function (button) {
-    // Проверяем, содержится ли кнопка в блоке с классом block-cheaper-container
-    if (
-      !button.closest(".block-cheaper-container") &&
-      !button.closest(".global-button-used")
-    ) {
-      // Если кнопка не содержится в блоке block-cheaper-container, убираем у нее текст
-      button.textContent = "";
-    }
-  });
-});
+//   // Перебираем каждую кнопку
+//   buttons.forEach(function (button) {
+//     // Проверяем, содержится ли кнопка в блоке с классом block-cheaper-container
+//     if (
+//       !button.closest(".block-cheaper-container") &&
+//       !button.closest(".global-button-used")
+//     ) {
+//       // Если кнопка не содержится в блоке block-cheaper-container, убираем у нее текст
+//       button.textContent = "";
+//     }
+//   });
+// });
