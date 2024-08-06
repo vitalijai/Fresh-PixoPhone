7b9445a6bbd69af9f690d024337f30f625634d70
ZigfeR <xxxpickxxx@gmail.com>
2024-08-06 14:40:46 +0300

    footer fix an home

---
+++ scripts/footer.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $(".footer-nav__title").on("click", function () {
+    // Проверяем ширину окна
+    if ($(window).width() < 719) {
+      // Находим родительский элемент footer-nav__column
+      var parent = $(this).closest(".footer-nav__column");
+
+      // Переключаем класс active у родительского элемента footer-nav__column
+      parent.toggleClass("active");
+
+      // Применяем slideToggle к элементу footer-nav__block внутри родительского элемента area__right
+      if (parent.hasClass("active")) {
+        parent.find(".footer-nav__block").slideDown().css("display", "flex");
+      } else {
+        parent.find(".footer-nav__block").slideUp();
+      }
+    }
+  });
+});
