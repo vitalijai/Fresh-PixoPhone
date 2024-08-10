dafe10a834c8e3de3e9e4dfdd4d292034a105997
ZigfeR <xxxpickxxx@gmail.com>
2024-08-10 19:58:38 +0300

    finished frequent questions

---
+++ scripts/frequent-questions.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $(".dropdown-area-block-header").on("click", function () {
+    // Найти соответствующий блок внутри текущего родительского блока
+    var $block = $(this)
+      .closest(".dropdown-area-frequent-questions")
+      .find(".dropdown-area-block");
+
+    // Проверка состояния блока
+    if (!$block.is(":visible")) {
+      // Выполнить slideDown и добавить класс open
+      $block.slideDown().css("display", "flex");
+      $(this).addClass("open");
+    } else {
+      // Если блок уже виден, выполнить slideUp и убрать класс open
+      $block.slideUp();
+      $(this).removeClass("open");
+    }
+  });
+});
