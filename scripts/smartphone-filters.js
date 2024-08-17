491b16bbd65b02d0441fa9ac2fe4f425c2966efe
ZigfeR <xxxpickxxx@gmail.com>
2024-08-17 11:23:49 +0300

    finish mobile smartphone-filters-content

--- scripts/smartphone-filters.js
+++ scripts/smartphone-filters.js
@@ -1,13 +1,17 @@
 $(document).ready(function () {
   // Добавляем эффекты slideDown/slideUp при наведении на блок .smartphone-filters-block
-  $(".smartphone-filters-block").hover(
-    function () {
-      // Когда курсор наведен на блок, плавно раскрываем dropdown внутри этого блока
-      $(this).find(".dropdown").stop(true, true).slideDown(300);
-    },
-    function () {
-      // Когда курсор уходит с блока, плавно скрываем dropdown внутри этого блока
-      $(this).find(".dropdown").stop(true, true).slideUp(300);
-    }
-  );
+  if ($(window).width() > 719) {
+    $(".smartphone-filters-block").hover(
+      function () {
+        // Проверяем ширину окна, выполняем код только если ширина больше 719px
+        // Когда курсор наведен на блок, плавно раскрываем dropdown внутри этого блока
+        $(this).find(".dropdown").stop(true, true).slideDown(300);
+      },
+      function () {
+        // Проверяем ширину окна, выполняем код только если ширина больше 719px
+        // Когда курсор уходит с блока, плавно скрываем dropdown внутри этого блока
+        $(this).find(".dropdown").stop(true, true).slideUp(300);
+      }
+    );
+  }
 });
